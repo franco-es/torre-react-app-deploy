3e7184e5a07eb547ef15c478eed8810cd72ecdfa
franco estrella <francoestrella55@gmail.com>
2021-07-07 00:31:27 -0300

    modify request to API, adding Content-Type

--- src/services/getUser.js
+++ src/services/getUser.js
@@ -5,6 +5,7 @@ export default function getUser(user) {
     axios({
       method: "GET",
       url: `https://torre-back-end.herokuapp.com/api/getUser?user=${user}`,
+      "Content-Type": "application/json",
     })
       .then((res) => {
         resolve(res.data);
